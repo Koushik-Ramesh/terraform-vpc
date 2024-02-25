50b9a5ea02d2720bc977e28093956d643bf06546
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-02-25 20:44:53 +0530

    Autopush

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 module "vpc" {
   source = "git::https://github.com/Koushik-Ramesh/tf-module-vpc.git?ref=main"
 }
+
+
+# We cannot parametrize anything that's added in the source
+# To limit that, we can use a tool called as Terrafile. All it does is, it clones the remote code fro the specified branch and make it locally available,
+
+
